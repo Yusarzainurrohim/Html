4cdafcab100e28a5a55dfe4df77f8b834163801b
Yusarzainurrohim <113924052+Yusarzainurrohim@users.noreply.github.com>
2022-10-07 09:54:46 +0700

    Add files via upload

---
+++ segitiga 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int luas,keliling;
+   int alas,tinggi,sisi;
+   cout<<"Aplikasi Hitung Luas + Keliling Segitiga"<<endl;
+   cout<<"Masukan Alas";cin>>alas;
+   cout<<"Masukan Tinggi";cin>>tinggi;
+   luas = 0.5 *alas * tinggi;
+   cout<<"Luas Segitiga=" <<luas; <<endl
+   cout<<"Masukan sisi"; cin >> sisi;
+ Keliling=sisi+sisi+sisi;
+   cout<<"Keliling Segitiga =" <<keliling;
+   }
+   
