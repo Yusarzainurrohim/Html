f32001ae77555c4972ef5d98abc988b63c0b6cea
Yusarzainurrohim <113924052+Yusarzainurrohim@users.noreply.github.com>
2022-10-07 10:08:36 +0700

    Add files via upload

---
+++ tes2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout<< "Aplikasi Penjumlahan"<<endl;
+  cout<< "Masukan Angka Pertama =";
+  int angka1 ;
+  cin>>angka1;
+  cout<< "Masukan Angka Kedua =";
+  int angka2;
+  cin>>angka2;
+  cout<< "Masukan Angka ketiga=";
+  int angka3;
+  cin>>angka3;
+  
+  int hasil;
+  hasil=(angka1 + angka2)/angka3;
+  cout<<"Hasil Perkalian ="<<hasil ;
+  }
