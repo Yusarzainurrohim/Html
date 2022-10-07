9ca68d767ade7d5cc58ca06daf73dad0c25c1a8b
Yusarzainurrohim <113924052+Yusarzainurrohim@users.noreply.github.com>
2022-10-07 09:03:28 +0700

    Add files via upload

---
+++ Kali.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout<< "Aplikasi Perkalian"<<endl;
+  cout<< "Masukan Angka Pertama =";
+  int angka1 ;
+  cin>>angka1;
+  cout<< "Masukan Angka Kedua =";
+  int angka2;
+  cin>>angka2;
+  
+  int hasil;
+  hasil = angka1 * angka2;
+  cout<<"Hasil Perkalian ="<<hasil ;
+  }
+  
