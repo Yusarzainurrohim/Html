a6eb8cc190a628951c0f012c5d6936ea0b3a53e3
Yusarzainurrohim <113924052+Yusarzainurrohim@users.noreply.github.com>
2022-10-07 10:09:26 +0700

    Add files via upload

---
+++ p.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int a,b,c;
+    for(a=5 ; a <=1; a--){
+      for(a=5;b<=a; b--){
+        printf("%1",a);  
+      }
+      printf("/n");
+    }
+}
